bbc634e99dc892a90d68b4343d35199b5f5fe788
trevthedev777 <bloodsportrecords@gmail.com>
2022-11-10 21:30:50 +0000

    Remove env.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-if os.path.isfile("env.py"):
-    import env
 import dj_database_url
 
 development = os.environ.get('DEVELOPMENT', False)
